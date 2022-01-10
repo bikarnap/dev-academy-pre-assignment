9300e353c6217a1a1d52372cc76e4286c82ae415
Bikarna Pokharel <bikarna.pokharel@tuni.fi>
2022-01-10 10:01:44 +0545

    Refactor to use farm data validation

--- farm-app-be/utils/uploadCsv.js
+++ farm-app-be/utils/uploadCsv.js
@@ -1,5 +1,12 @@
 const csvtojson = require('csvtojson')
 const Farm = require('../models/farm')
+const mongoose = require('mongoose')
+const config = require('./config')
+const validateFarmData = require('./validateFarmData')
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => console.log('connected to MongoDB successfully'))
+  .catch(err => console.log(`connection to MongoDB failed\n${err.message}`))
 
 const uploadCsvs = (...csvs) => {
   csvs.map(csv => {
@@ -7,17 +14,28 @@ const uploadCsvs = (...csvs) => {
       .fromFile(csv)
       .then(farmArray => {
         farmArray.map(farm => {
-          const newFarm = new Farm({
-            location: farm.location,
-            datetime: farm.datetime,
-            sensorType: farm.sensorType,
-            value: farm.value
-          })
-
-          newFarm.save()
-            .then(savedFarm => console.log(savedFarm))
-            .catch(err => console.log(err.message))
+          if (validateFarmData(farm.sensorType, farm.value)) {
+            const newFarm = new Farm({
+              location: farm.location,
+              datetime: farm.datetime,
+              sensorType: farm.sensorType,
+              value: farm.value
+            })
+  
+            newFarm.save()
+              // .then(savedFarm => console.log(savedFarm))
+              .catch(err => console.log(err.message))
+          } else {
+            console.log(farm.sensorType, farm.value)
+          }
         })
       })
   })
 }
+const arguments = process.argv.slice(2)
+console.log(arguments)
+const csvs = ['Nooras_farm.csv', 'PartialTech.csv', 'ossi_farm.csv', 'friman_metsola.csv']
+if (arguments.length > 0)
+  uploadCsvs(...arguments)
+else
+  uploadCsvs(...csvs)
