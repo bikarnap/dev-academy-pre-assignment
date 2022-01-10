1dbc3cd63a983b5cc204699963eed607e47e8e21
Bikarna Pokharel <bikarna.pokharel@tuni.fi>
2022-01-10 16:02:33 +0545

    Establish connection to the database

--- farm-app-be/app.js
+++ farm-app-be/app.js
@@ -1,6 +1,12 @@
 const farmsRouter = require('./controllers/farms')
+const config = require('./utils/config')
+const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => console.log(`connected to MongoDB successfully`))
+  .catch(err => console.log(`connection to MongoDB failed\n${err.message}`))
 
 app.use('/api/farms', farmsRouter)
 
