a8bfe3be1b99d6ff04a2393f2c1aea3820c0d285
Bikarna Pokharel <bikarna.pokharel@tuni.fi>
2022-01-10 16:01:33 +0545

    Implement GET endpoint to get farms /api/farms returns farm data limited to 100 results. /api/farms?sensorType=<sensorType> returns farm data with given sensor types limited to 100 results

--- farm-app-be/controllers/farms.js
+++ farm-app-be/controllers/farms.js
@@ -1,7 +1,36 @@
 const farmsRouter = require('express').Router()
+const Farm = require('../models/farm')
 
 farmsRouter.get('/', (req, res) => {
-  res.json({ message: 'farms app backend' })
+  const { sensorType } = req.query
+  if (sensorType) {
+    Farm.find({ sensorType: sensorType })
+      .limit(100)
+      .then(farms => {
+        if (farms.length > 0)
+          res.json({ data: farms })
+        else
+          res.status(404)
+            .send({ 
+              error: `sensorType=${sensorType} not valid`,
+              validSensors: ['temperature', 'rainFall', 'pH']
+            })
+      })
+      .catch(err => console.log(err.message))
+  } else {
+    Farm.find({})
+      .limit(100)
+      .then(farms => {
+        if (farms.length > 0)
+          res.json({ data: farms })
+        else
+          res.status(404)
+            .send({ 
+              error: `Data not found`
+            })
+      })
+      .catch(err => console.log(err.message))
+  }
 })
 
 module.exports = farmsRouter
