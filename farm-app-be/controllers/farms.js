7797c16af08e6d8d649c97a7c89e76132f450388
Bikarna Pokharel <bikarna.pokharel@tuni.fi>
2022-01-11 14:38:24 +0545

    Refactor to use pagination mongoose-paginate-v2 packages used to paginate the api response

--- farm-app-be/controllers/farms.js
+++ farm-app-be/controllers/farms.js
@@ -1,35 +1,50 @@
 const farmsRouter = require('express').Router()
 const Farm = require('../models/farm')
 
-farmsRouter.get('/', (req, res) => {
-  const { sensorType } = req.query
+farmsRouter.get('/', async (req, res) => {
+  const { sensorType, page, limit } = req.query
+  const options = {
+    page: parseInt(page) || 1,
+    limit: parseInt(limit) || 100
+  }
+
   if (sensorType) {
-    Farm.find({ sensorType: sensorType })
-      .limit(100)
-      .then(farms => {
-        if (farms.length > 0)
-          res.json({ data: farms })
-        else
-          res.status(404)
-            .send({ 
-              error: `sensorType=${sensorType} not valid`,
-              validSensors: ['temperature', 'rainFall', 'pH']
-            })
-      })
-      .catch(err => console.log(err.message))
+    const farms = await Farm.paginate({ sensorType: sensorType }, options)
+    if (farms) {
+      let nextCursor = null
+      if (farms.hasNextPage) {
+        nextCursor = `/api/farms?sensorType=${sensorType}&page=${farms.nextPage}&limit=${farms.limit}`
+      } else {
+        nextCursor = null
+      }
+      
+      if (nextCursor) {
+        res.status(200)
+        .json({ nextCursor, farms }) 
+      } else {
+        res.status(200)
+        .json(farms) 
+      }
+     
+    }
   } else {
-    Farm.find({})
-      .limit(100)
-      .then(farms => {
-        if (farms.length > 0)
-          res.json({ data: farms })
-        else
-          res.status(404)
-            .send({ 
-              error: `Data not found`
-            })
-      })
-      .catch(err => console.log(err.message))
+    const farms = await Farm.paginate({}, options)
+    if (farms) {
+      let nextCursor = `/api/farms?page=${farms.nextPage}&limit=${farms.limit}`
+      if (farms.hasNextPage) {
+        nextCursor = `/api/farms?page=${farms.nextPage}&limit=${farms.limit}`
+      } else {
+        nextCursor = null
+      }
+      
+      if (nextCursor) {
+        res.status(200)
+        .json({ nextCursor, farms }) 
+      } else {
+        res.status(200)
+        .json(farms) 
+      }
+    }
   }
 })
 
