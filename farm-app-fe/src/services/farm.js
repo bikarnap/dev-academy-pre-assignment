bbb95cc67e492decd76712eb0744f21dc498f6a3
Bikarna Pokharel <bikarna.pokharel@tuni.fi>
2022-01-13 18:01:00 +0545

    Refactor getAll function getAll function name changed to getFarms that accepts two arguments - page and limit. page is the page number of api results from backend and limit is the number of farms to be returned per page

--- farm-app-fe/src/services/farm.js
+++ farm-app-fe/src/services/farm.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
 const baseUrl = 'http://localhost:3001/api/farms'
-const getAll = () => {
-  const request = axios.get(baseUrl)
+const getFarms = (page, limit) => {
+  const request = axios
+    .get(`${baseUrl}?page=${page}&limit=${limit}`)
   return request.then(res => res.data)
 }
 
-const farmService = { getAll }
-
+const farmService = { getFarms }
 export default farmService
