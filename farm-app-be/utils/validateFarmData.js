728c9809cac21160dc7fc4f2086f995ddd41bf72
Bikarna Pokharel <bikarna.pokharel@tuni.fi>
2022-01-16 22:20:51 +0545

    Refactor to fix swith/case statments

--- farm-app-be/utils/validateFarmData.js
+++ farm-app-be/utils/validateFarmData.js
@@ -1,24 +1,24 @@
 const validateFarmData = (sensorType, value) => {
   switch (sensorType.toLowerCase()) {
-    case 'temperature':
-      if (value >= -50 && value <= 100)
-        return true
-      else
-        return false
-    case 'rainfall':
-      if (value >= 0 && value <= 500)
-        return true
-      else
-        return false
-    case 'ph':
-      if (value >= 0.0 && value <= 14.0)
-        return true
-      else
-        return false
-    default:
-      console.log(`Sensor type '${sensorType}' is not valid`)
+  case 'temperature':
+    if (value >= -50 && value <= 100)
+      return true
+    else
       return false
-      break
+  case 'rainfall':
+    if (value >= 0 && value <= 500)
+      return true
+    else
+      return false
+  case 'ph':
+    if (value >= 0.0 && value <= 14.0)
+      return true
+    else
+      return false
+  default:
+    console.log(`Sensor type '${sensorType}' is not valid`)
+    return false
+    break
   }
 }
 
