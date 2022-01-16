4dfbba8dd76295cf3b86c164c6b7b9cd24ee8552
Bikarna Pokharel <bikarna.pokharel@tuni.fi>
2022-01-16 22:26:15 +0545

    Refactor

--- farm-app-be/app.js
+++ farm-app-be/app.js
@@ -9,7 +9,7 @@ app.use(cors())
 app.use(express.json())
 
 mongoose.connect(config.MONGODB_URI)
-  .then(() => console.log(`connected to MongoDB successfully`))
+  .then(() => console.log('connected to MongoDB successfully'))
   .catch(err => console.log(`connection to MongoDB failed\n${err.message}`))
 
 app.use(middleware.requestLogger)
