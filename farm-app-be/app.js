1fb5c79e8ae9dd194477c5ca0b778a81c1737280
Bikarna Pokharel <bikarna.pokharel@tuni.fi>
2022-01-16 12:53:09 +0545

    Use middleware requestLogger middleware unknownEndpoint middleware errorHandler middleware

--- farm-app-be/app.js
+++ farm-app-be/app.js
@@ -1,13 +1,22 @@
 const farmsRouter = require('./controllers/farms')
 const config = require('./utils/config')
+const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 const express = require('express')
+const cors = require('cors')
 const app = express()
+app.use(cors())
+app.use(express.json())
 
 mongoose.connect(config.MONGODB_URI)
   .then(() => console.log(`connected to MongoDB successfully`))
   .catch(err => console.log(`connection to MongoDB failed\n${err.message}`))
 
+app.use(middleware.requestLogger)
+
 app.use('/api/farms', farmsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
